d79cbb5a12aeca5d5f5ab255df04f7f47e57527e
Sanket Shirsath <sanketshirsath226@gmail.com>
2024-01-20 17:41:41 +0530

    Initial Push For Otp Screen

--- services/MapService.js
+++ services/MapService.js
@@ -2,6 +2,5 @@ import * as MapView from 'react-native-maps';
 
 export const getDirections = async (origin, destination) => {
   // Use a map library's directions API
-  const directions = await MapView.getDirections({ origin, destination });
-  return directions;
+  return await MapView.getDirections({origin, destination});
 };
